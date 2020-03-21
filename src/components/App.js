d0c833e02b9cfcd707870ff2dbd7e70dd6682e3c
minecodebot <minecode.geral@gmail.com>
2020-03-21 11:09:08 +0000

    deploy: minecode/website@a846c34b03b2bf6575ceb2d5b2272136d4f13aa4

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 class App extends React.Component {
     render() {
         return (
-          <Router basename="/">
+          <Router basename="/website">
             <div>
                 <Navbar />
                 <main>
