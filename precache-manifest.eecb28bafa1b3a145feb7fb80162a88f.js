d6cc61a5ae6968d6d9ea5f5b6b6741741c146bde
minecodebot <minecode.geral@gmail.com>
2020-03-31 20:22:08 +0000

    deploy: minecode/code_contest@f1d0fe7f0f4f580870425476387cd9d484b60964

---
+++ precache-manifest.eecb28bafa1b3a145feb7fb80162a88f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d982d3471e281b135e725eadfe0af537",
+    "url": "/code_contest/index.html"
+  },
+  {
+    "revision": "98007402b15c20b9a3d7",
+    "url": "/code_contest/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "89ccd24d1ae4313d0eca",
+    "url": "/code_contest/static/css/main.630560e7.chunk.css"
+  },
+  {
+    "revision": "98007402b15c20b9a3d7",
+    "url": "/code_contest/static/js/2.763e4956.chunk.js"
+  },
+  {
+    "revision": "dfe895dedb56c105ed91eb22a78a7699",
+    "url": "/code_contest/static/js/2.763e4956.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89ccd24d1ae4313d0eca",
+    "url": "/code_contest/static/js/main.e18e665c.chunk.js"
+  },
+  {
+    "revision": "90cdb190491d96cd284c",
+    "url": "/code_contest/static/js/runtime-main.70798011.js"
+  }
+]);
