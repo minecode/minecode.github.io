49d2b76add888dbb3b3e589992ace86f9211d445
minecodebot <minecode.geral@gmail.com>
2020-04-20 23:41:23 +0000

    deploy: minecode/website@2d006d113788b21cf74f6d014523d26610035eea

---
+++ precache-manifest.9b5b81c966d6772abad563e0e34e8a0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8002006dcab401dd15ac21557c6d6b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf83c7bbbf085a6ed887",
+    "url": "/static/css/main.813f1fe2.chunk.css"
+  },
+  {
+    "revision": "59b731495a00bbb6ce08",
+    "url": "/static/js/2.044ebad7.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.044ebad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf83c7bbbf085a6ed887",
+    "url": "/static/js/main.36fbaf51.chunk.js"
+  },
+  {
+    "revision": "2b16e05acbb0eb80e55c",
+    "url": "/static/js/runtime-main.2b2cda15.js"
+  }
+]);
